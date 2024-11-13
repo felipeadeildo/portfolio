c49760a2770cef1f7fa16bc509cdf31ccae6537a
Felipe Adeildo <contato@felipeadeildo.com>
2024-11-13 13:12:23 -0300

    feat: nuxt application base

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <div class="text-2xl text-center">Site do Lipe</div>
+
+    <div class="fixed right-4 bottom-3">
+      <ThemeToggle />
+    </div>
+  </div>
+</template>
---
+++ components/ThemeToggle.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  const colorMode = useColorMode()
+  const isDark = computed({
+    get() {
+      return colorMode.value === 'dark'
+    },
+    set() {
+      colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+    },
+  })
+</script>
+
+<template>
+  <ClientOnly>
+    <Button variant="ghost" aria-label="Theme" @click="isDark = !isDark">
+      <Icon
+        :name="
+          isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'
+        "
+        class="size-5"
+      />
+    </Button>
+    <template #fallback>
+      <div class="size-5" />
+    </template>
+  </ClientOnly>
+</template>
